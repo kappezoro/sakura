59130ef5f8985e79ec7b674b27f90fc5a401ddd5
Atsuko Tsujioka <atsuko.tsujioka@zozo.com>
2021-03-04 10:07:28 +0900

    commit:

--- src/main/kotlin/com/example/sakura/SakuraApplication.kt
+++ src/main/kotlin/com/example/sakura/SakuraApplication.kt
@@ -2,25 +2,26 @@ package com.example.sakura
 
 import com.example.sakura.repository.SakuraRepository
 import com.example.sakura.service.SakuraService
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.Paths
+import org.springframework.jdbc.core.JdbcTemplate
+import java.io.File
 
 @SpringBootApplication
-class SakuraApplication
-
-	@Autowired
-	private lateinit var service: SakuraService
-
-	@Autowired
-	private lateinit var repository: SakuraRepository
-
-fun main() {
-		val query = service.getSqlQuery("src/main/resources/sql/check_staff_duplicate.sql")
-		val result = repository.count(query);
-		print(result)
-		//service.allUsers
+class SakuraApplication(val jdbcTemplate: JdbcTemplate) : CommandLineRunner {
+	override fun run(vararg args: String?) {
+		val targetName = args[0]
+		val fileList = File("src/main/resources/sql").listFiles()
+		val service = SakuraService()
+		val repository = SakuraRepository(jdbcTemplate)
+		for(file in fileList) {
+			print(repository.count(service.getSqlQuery(file.absolutePath), targetName))
+		}
 	}
+}
+
+fun main(args: Array<out String?>) {
+	SpringApplication.run(SakuraApplication::class.java, *args)
+}
 
--- src/main/kotlin/com/example/sakura/repository/SakuraRepository.kt
+++ src/main/kotlin/com/example/sakura/repository/SakuraRepository.kt
@@ -3,12 +3,12 @@ package com.example.sakura.repository
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.stereotype.Repository
 
+
 @Repository
-class SakuraRepository(private val jdbcTemplate: JdbcTemplate? = null) {
+class SakuraRepository(private val jdbcTemplate: JdbcTemplate) {
 
-
-    fun count(sql:String):Int{
+    fun count(sql:String, targetName:String?):Int {
         return jdbcTemplate!!.queryForObject(sql, Int::class.java)!!
     }
-
+    
 }
